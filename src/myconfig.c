fda0cbb87f81190964cb344f6eb43afb312ced75
shiliang <liangshi@hustunique.com>
2016-03-19 20:03:46 +0800

    avoid secured shebang problem more graceful
    Conflicts:
    	src/myconfig.c
    	src/mystate.c

--- src/myconfig.c
+++ src/myconfig.c
@@ -19,7 +19,6 @@ static const char *PACKAGE_BUGREPORT = "http://code.google.com/p/mentohust/issue
 #include "myini.h"
 #include "myfunc.h"
 #include "dlfunc.h"
-#include "yashargs.h"
 #include "util.h"
 #include <string.h>
 #include <arpa/inet.h>
@@ -68,7 +67,6 @@ char nic[NIC_SIZE] = "";	/* 发出认证数据包的网卡名（WAN） */
 char nicLan[NIC_SIZE] = "";	/* 监听认证数据包的网卡名（LAN，仅在代理模式下使用） */
 char dataFile[MAX_PATH] = "";	/* 数据文件 */
 char dhcpScript[MAX_PATH] = "";	/* DHCP脚本 */
-char **dhcpArguements;
 u_int32_t ip = 0;	/* 本机IP */
 u_int32_t mask = 0;	/* 子网掩码 */
 u_int32_t gateway = 0;	/* 网关 */
@@ -564,12 +562,7 @@ static void printConfig()
 	if (dhcpMode != 0)
     {
 		printf(_("** DHCP脚本:\t"));
-        dhcpArguements = yash_args(dhcpScript);
-        for(tmp = dhcpArguements; tmp != 0 && *tmp != 0; tmp++)
-        {
-            printf("%s ", *tmp);
-        }
-        putchar('\n');
+        printf(dhcpScript);
     }
 }
 
